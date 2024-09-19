a724e206bda0e1801ad1a37505479858e9f4ad52
BogdanLi <li.bogdan44@gmail.com>
2024-09-19 19:56:32 +0500

    added searchbar add btn and tasks table

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Todo from "./components/Todo";
 
 const App = () => {
   return (
-    <main className="h-screen grid place-items-center">
+    <main className="h-screen grid place-items-center mx-64">
       <Todo />
     </main>
   );
